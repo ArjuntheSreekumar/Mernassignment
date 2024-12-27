efc1865aec6271e3430007feeff2b7492f1e282c
Arjun Sreekumar <134309199+ArjuntheSreekumar@users.noreply.github.com>
2024-12-28 01:33:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,48 @@
 import express from 'express';
+import dotenv from 'dotenv'; //.env file to keep sensitive information as db connection info
 import mongoose from 'mongoose';
-import cors from 'cors'; // CSRF protection
+import cors from  'cors';  // cors -> cross site resource sharing (csrf) forgery protection
 import cookieParser from 'cookie-parser';
 
-import authRoute from './routes/auth.js';
-import tourRoute from './routes/tours.js';
-import userRoute from './routes/users.js';
+import authRoute from './routes/auth.js'
+import tourRoute from './routes/tours.js'
+import userRoute from './routes/users.js'
 import reviewRoute from './routes/reviews.js';
 import bookingRoute from './routes/bookings.js';
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8000;
+// //for testing
 
-const app = express();
-const port = 8000; // Use the desired port directly
+// app.get('/',(req , res)=>{
+//     res.send("api is working");
+// })
 
-// MongoDB Connection URI// Updated MongoDB Connection URI
-const MONGO_URI = 'mongodb+srv://newaccount101:arjun_2004@cluster0.cf5gq.mongodb.net/tour-booking?retryWrites=true&w=majority';
+//database connection
+mongoose.set("strictQuery", false);
+const connect =  async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser : true,
+            useUnifiedTopology : true
+        })
+        console.log("MongoDB database connected..")
+    }
+    catch(err){
+        console.log("MongoDB database connection failed..")
+    }
+}
 
-
-// Database connection
-mongoose.set('strictQuery', false);
-const connect = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB database connected..');
-  } catch (err) {
-    console.log('MongoDB database connection failed:', err.message);
-  }
-};
-
-// Middleware
+// middleware
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
-// Routes
-app.use('/auth', authRoute);
-app.use('/tours', tourRoute);
-app.use('/users', userRoute);
-app.use('/review', reviewRoute);
-app.use('/booking', bookingRoute);
-
-// Start the server
-app.listen(port, () => {
-  connect();
-  console.log('Server listening on port', port);
+app.use('/auth', authRoute )
+app.use('/tours', tourRoute )
+app.use('/users', userRoute )
+app.use('/review',reviewRoute)
+app.use('/booking',bookingRoute)
+app.listen(port , () =>{
+    connect();
+    console.log("server listening on port", port);
 });
